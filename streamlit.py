d03e13c2607319589121a0e7bb14995221d21246
fika4life <fistewie@gmail.com>
2021-05-02 08:50:33 +0200

    adds plotly chart

--- streamlit.py
+++ streamlit.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
+import plotly.express as px
 
 
 df_sm = pd.read_csv('player_stats_sm.csv', index_col=0)
@@ -36,14 +36,23 @@ filtered_df = df_sm[(df_sm['round'].astype(
     'int64').isin(filter_by_weeks(weeks, df_sm)))]
 filtered_df = filtered_df[filtered_df['position_name'] == position]
 # which goalie has hightest total poiints
-total_points_by_pos = filtered_df.groupby(['name', 'team_name'])[
+total_points_by_pos = filtered_df.groupby(['element', 'name', 'team_name'])[
     'total_points'].sum()
 tp_by_points = total_points_by_pos[total_points_by_pos > 10]
 tp_by_points = tp_by_points.sort_values(ascending=False)
+tp_by_points = tp_by_points.to_frame().reset_index().set_index('element')
 
-
+#tp_by_points_chart = tp_by_points.drop('element', axis=1)
 st.write(tp_by_points)
 
 # data wrangle for charts
-tp_by_points
-st.line_chart(tp_by_points)
+top_ids = tp_by_points.index.to_list()
+top_ids = top_ids[0:5]
+
+df_top = filtered_df[filtered_df['element'].isin(top_ids)]
+
+# plot
+fig = px.line(df_top, x='round', y='total_points',
+              color='name', hover_name='name', title='Performance of top players by week')
+
+st.plotly_chart(fig, use_container_width=True)
